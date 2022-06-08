c0e0b11115a126db9238a287f3079114ebaf3d75
rodrigomgrana <rodrigograna@live.com.ar>
2022-06-08 16:55:24 -0300

    ultimo cambio

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.use ("/static", express.static("/public"))
 app.listen (3001, ()=>{console.log("servidor escuchando puerto 3001")})
 
 app.get ("/", (req,res) =>{
-    res.sendFile (__dirname + "/views/home.html")
+    res.sendFile (__dirname + "/views/index.html")
 })
