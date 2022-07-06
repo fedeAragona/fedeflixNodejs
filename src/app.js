66df7a46c8cfe2ac933080026c933f96f4774d46
rodrigomgrana <rodrigograna@live.com.ar>
2022-07-06 19:42:15 -0300

    06/06

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ const app = express();
 
 const path = require("path");
 
+const methodOverride= require("method-override");
 
 app.listen(3030, () => console.log("Trabajando en puerto 3030"));
 
-
 app.set('view engine','ejs');
 
-
 app.use(express.static(path.join(__dirname,'../public')));
-
 
 const routerMain = require('./routes/main');
 
 app.use(routerMain);
+
+app.use(methodOverride("_method"));
